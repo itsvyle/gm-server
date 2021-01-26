dc218d85205ae6d86bfae35fecad098391718e0f
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-01-26 11:19:54 -0800

    Update flags.js

--- flags.js
+++ flags.js
@@ -1,16 +1,18 @@
-var Flags = function(vals,flags_) {
-    if (typeof(vals) !== "object" || !vals) {throw "'vals' must be an object";}
+class Flags {
+    constructor(vals,flags_) {
+        if (typeof(vals) !== "object" || !vals) {throw "'vals' must be an object";}
     
-    this.flag_values = {};
-    for(var n_ in vals) {
-        var v_ = vals[n_];
-        if (typeof(v_) !== "number") {continue;}
-        this.flag_values[n_] = (1 << v_);
+        this.flag_values = {};
+        for(var n_ in vals) {
+            var v_ = vals[n_];
+            if (typeof(v_) !== "number") {continue;}
+            this.flag_values[n_] = (1 << v_);
+        }
+        
+        this.flags = (typeof(flags_) === "number") ? flags_ : 0;
     }
-    
-    this.flags = (typeof(flags_) === "number") ? flags_ : 0;
-    
-    this.add = function(flag) {
+
+    add(flag) {
         var par = this;
         if (Array.isArray(flag)) {
             return flag.every(function (f) {
@@ -21,9 +23,9 @@ var Flags = function(vals,flags_) {
         if (!flag in this.flag_values) {return false;}
         this.flags |= this.flag_values[flag];
         return true;
-    };
-    
-    this.has = function (flag) {
+    }
+
+    has(flag) {
         var par = this;
         if (Array.isArray(flag)) {
             return flag.every(function (f) {
@@ -34,9 +36,9 @@ var Flags = function(vals,flags_) {
         if (!flag in this.flag_values) {return false;}
         flag = this.flag_values[flag];
         return ((this.flags & flag) === flag);
-    };
-    
-    this.remove = function (flag) {
+    }
+
+    remove(flag) {
         var par = this;
         if (Array.isArray(flag)) {
             return flag.every(function (f) {
@@ -48,54 +50,54 @@ var Flags = function(vals,flags_) {
         flag = this.flag_values[flag];
         this.flags &= ~flag;
         return true;
-    };
-    
-    this.array = function () {
+    }
+
+    array() {
         var r = [];
         for(var f in this.flag_values) {
             if (this.has(f)) {r.push(f);}
         }
         return r;
-    };
-    
-    this.object = function () {
+    }
+
+    object() {
         var r = {};
         for(var f in this.flag_values) {
             r[f] = this.has(f);
         }
         return r;
-    };
-    
-    this.setFlags = function (f) {
+    }
+
+    setFlags(f) {
         if (typeof(f) !== "number") {throw("'f' must be a number");}
         this.flags = f;
-    };
-    
-    this.set = function (flag,val) {
+    }
+
+    set(flag,val) {
         if (val === true) {
             this.add(flag);
         } else if (val === false) {
             this.remove(flag);
         }
-    };
-    
-    this.fromObject = function (o) {
+    }
+
+    fromObject(o) {
         if (!o || typeof(o) !== "object") {return false;}
         for(var n in o) {
             if (!(n in this.flag_values)) {continue;}
             this.set(n,o[n]);
         }
-    };
-    
-    this.addAll = function () {
+    }
+
+    addall() {
         for(var f in this.flag_values) {
             this.flags |= this.flag_values[f];
         }
-    };
-    
-    this.toString = function () {
+    }
+
+    toString() {
         return String(this.flags);
-    };
+    }
 }
 
 module.exports = Flags;
