ca6728599363e2cbb452c7003247c31100a299eb
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-01-28 08:06:15 -0800

    Update util.js

--- util.js
+++ util.js
@@ -78,26 +78,61 @@ module.exports = {
         return res;
     },
     projectURL: "https://remote.lfny.repl.co",
-    request: function (request,opts,clb) {
+    request: function (request,url,opts,clb) {
         if (typeof(clb) !== "function") clb = () => {};
         if (!request) return clb({status: 0,error: "Request must be an imported request npm instance"});
-        if (!opts.url) return clb({status: 0,error: "'opts' must be an object with a 'url' property"});
-        let url = opts.url;
-        delete opts['url'];
-        request(url,opts,function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-				clb({
-					status: 1,
-					code: response.statusCode,
-					body: body
-				});
-			} else {
-				clb({
-					status: 0,
-					error: error || body,
-					code: response.statusCode
-				});
-			}
+        if (!opts || typeof(opts) !== "object") opts = {};
+        
+        if (typeof(opts.headers) != "object") {
+            opts.headers = {
+                "content-type": "application/x-www-form-urlencoded"
+            };
+        } else {
+            if (!opts.headers['content-type']) {
+                opts.headers['content-type'] = "application/x-www-form-urlencoded";
+            }
+        }
+
+        if (!Array.isArray(opts.accept_codes)) {opts.accept_codes = [200];}
+
+        if (!opts.method) {opts.method = "GET";}
+
+        let options = {};
+        options.headers = opts.headers;
+        if (!!opts.body) {
+            options.body = opts.body;
+        }
+        if (!!opts.extra_attributes && typeof(opts.extra_attributes) == "object") {
+            options = Object.assign(options,opts.extra_attributes);
+        }
+
+        request(url,options,function (error, response, body) {
+                var r = {status: null,http_code: response.statusCode,res: null,error_level: 0,error: null};
+                r.headers = response.headers;
+                
+                if (error) {
+                    return clb({status: 0,error_level: 2,error: "Error making request: " + error});
+                }
+
+                if (opts.accept_codes.includes(response.statusCode) === false) {
+                    r.status = 0;
+                    r.error_level = 1;
+                    r.error = "Error making request(" + String(r.http_code) + ": " + body + ")";
+                } else {
+                    r.status = 1;
+                    if (opts.json === true) {
+                        r.res = JSONParse(body);
+                        if (r.res === null) {
+                            r.status = 0;
+							r.res = null;
+                            r.error_level = 2;
+							r.error = "Error reading json response";
+                        }
+                    } else {
+                        r.res = body;
+                    }
+                }
+                clb(r);
         });
     },
     sortBy: function() {
@@ -139,75 +174,20 @@ module.exports = {
             .replace(/'/g, "&#039;");
     },
     formatTime: function (milliseconds) {
-		if (typeof(milliseconds) != "number") {return milliseconds;}
-		if (milliseconds >= (3600 * 24 * 1000)) {//more than a day
-		    return String(Math.floor(milliseconds / (1000 * 60 * 60 * 24))) + "d " + String(Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) + "h " + String(Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))) + "m " + Math.floor((milliseconds % (1000 * 60)) / 1000) + "s";
-		} else if (milliseconds >= 3600 * 1000) {
-		    return String(Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) + "h " + String(Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60)))+ "m " + String(Math.floor((milliseconds % (1000 * 60)) / 1000)) + "s";
-		} else if (milliseconds >= 60 * 1000) {
-		    return String(Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))) + "m " + String(Math.floor((milliseconds % (1000 * 60)) / 1000)) + "s";
-		} else {
-		    return String(Math.round(milliseconds / 1000)) + "s";
-		}
-},
+        if (typeof(milliseconds) != "number") {return milliseconds;}
+        if (milliseconds >= (3600 * 24 * 1000)) {//more than a day
+            return `${Math.floor(milliseconds / (1000 * 60 * 60 * 24))}d ${Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}h ${Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))}m ${Math.floor((milliseconds % (1000 * 60)) / 1000)}s`;
+        } else if (milliseconds >= 3600 * 1000) {
+            return `${Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}h ${Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))}m ${Math.floor((milliseconds % (1000 * 60)) / 1000)}s`;
+        } else if (milliseconds >= 60 * 1000) {
+            return `${Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))}m ${Math.floor((milliseconds % (1000 * 60)) / 1000)}s`;
+        } else {
+            return String(milliseconds) + "s";
+        }
+    },
 	UTCTime: function (d1) {
 		if (!d1) {d1 = new Date();}
         var now = new Date(d1.getUTCFullYear(),d1.getUTCMonth(),d1.getUTCDate(),d1.getUTCHours(),d1.getUTCMinutes(),d1.getUTCSeconds(),d1.getUTCMilliseconds());
         return now.getTime();
-	},
-    deepEqual: function (object1, object2) {
-        var isObject = function (object) {return (object != null && typeof object === 'object');};
-        var keys1 = Object.keys(object1);
-        var keys2 = Object.keys(object2);
-
-        if (keys1.length !== keys2.length) {
-            return false;
-        }
-        for(var i = 0;i < keys1.length;i++) {
-            var key = keys1[i];
-            var val1 = object1[key];
-            var val2 = object2[key];
-            var areObjects = isObject(val1) && isObject(val2);
-            if (areObjects && !gm.deepEqual(val1, val2) || !areObjects && val1 !== val2
-            ) {return false;}
-        }
-        return true;
-    },
-    generateID: function (length) {
-        var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    },
-    express: function ({port,post,express}) {
-        if (!express) express = require("express");
-        if (!port) port = 3000;
-        if (!post) post = false;
-
-        var app = express();
-        if (post === true) {
-            var bodyParser = require('body-parser');
-            app.use(bodyParser.json({limit: "50mb"}));
-            app.use(bodyParser.urlencoded({limit: "50mb",extended: true}));
-        }
-        app.connect = function (clb) {
-            if (clb === true) clb = () => {console.log("App listening at port: " + port);};
-            if (typeof(clb) !== "function") {clb = () => {};}
-            app.listen(port,() => {clb();});
-        };
-        return app;
-    },
-    parseCookies:function (rc) {
-        var list = {};//rc = request.headers.cookie;
-
-        rc && rc.split(';').forEach(function( cookie ) {
-            var parts = cookie.split('=');
-            list[parts.shift().trim()] = decodeURI(parts.join('='));
-        });
-
-        return list;
-    }
+	}
 };
