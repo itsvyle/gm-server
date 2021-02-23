33ed300c5740c78505f17ff37b336906f1ecdbf6
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-02-23 14:53:34 -0800

    Add files via upload

---
+++ simple-db/add.js
@@ -0,0 +1,341 @@
+var firstSave,not,base,field_names,key_names;
+function onload() {
+    not = new gm.NotificationMessages();
+    base = gm.base();
+    field_names = base.fields.map(function (f) {return f.name;});
+    //key_names = base.keyNames.map(function (f) {return f.name;});
+
+    firstSave = toSave();
+    refresh();
+}
+//fields
+function toSaveFields() {
+    var trs = document.getElementsByClassName("row-tr");
+    var r = [],i,j,jm = field_names.length,im = trs.length,n,t,bf,t,v,e,valid,tr;
+    for(i = 0;i < im;i++) {
+        tr = trs[i];
+        var fields = {};
+        valid = true;
+        for(j = 0;j < jm;j++) {
+            var gotErr = "";
+            n = field_names[j];
+            t = tr.getElementsByClassName("field-" + n)[0];
+            if (!t) {continue;}
+            bf = base.fields[j];
+            if (!bf) {continue;}
+            //fields[n] = t;
+            v = t.value;
+            e = t.nextSibling;
+            if (!v && bf.required && bf.type !== "boolean") {
+                gotErr = "This field is required";
+            }
+            if (!gotErr) {
+                if (bf.type === "number") {
+                    v = gm.parseInt(v);
+                    if (v === null) {
+                        gotErr = "This field must contain a number"
+                    }
+                } else if (bf.type === "boolean") {
+                    v = t.checked;
+                }
+            }
+            if (bf.values) {
+                if (!bf.values.includes(v)) {
+                    gotErr = "This field only accepts the following values: ['" + bf.values.join("', '") + "']";
+                }
+            }
+            
+            if (gotErr) {
+                if (!t.className.includes("invalid")) {t.className += " invalid";}
+                e.innerText = gotErr;
+                if (!e.className.includes("visible")) {e.className += " visible";}
+                valid = false;
+            } else {
+                if (e.className.includes("visible")) {e.className = e.className.replaceAll("visible","").trim();}
+                if (t.className.includes("invalid")) {t.className = t.className.replaceAll("invalid","").trim();}
+                fields[n] = v;
+            }
+        }
+        if (!valid) {
+            if (!tr.className.includes("invalid-row")) {
+                tr.className += " invalid-row";
+                tr.title = "This row is not valid and will not be saved";
+            }
+        } else {
+            tr.className = tr.className.replaceAll("invalid-row","").trim();
+            tr.title = "";
+            r.push(fields);
+        }
+    }
+    return r;
+}
+
+//keys
+function toSaveKeys() {
+    var container = document.getElementById("keys-container");
+    var r = {},i,im = base.keyNames.length,n,t,bf,t,v,e,valid,tr;
+    for(i = 0;i < im;i++) {
+        var gotErr = "";
+        n = base.keyNames[i];
+        bf = n;
+        n = n.name;
+        t = container.getElementsByClassName("key-" + n)[0];
+        if (!t) {continue;}
+        v = t.value;
+        e = t.nextSibling;
+        if (!v && bf.required && bf.type !== "boolean") {
+            gotErr = "This field is required";
+        }
+        if (!gotErr) {
+            if (bf.type === "number") {
+                if (!!v) {
+                    v = gm.parseInt(v);
+                    if (v === null) {
+                        gotErr = "This field must be a number"
+                    }
+                } else {
+                    v = null;
+                }
+            } else if (bf.type === "boolean") {
+                v = t.checked;
+            }
+        }
+        if (bf.values) {
+            if (!bf.values.includes(v)) {
+                gotErr = "This field only accepts the following values: ['" + bf.values.join("', '") + "']";
+            }
+        }
+        if (gotErr) {
+            if (!t.className.includes("invalid")) {t.className += " invalid";}
+            e.innerText = gotErr;
+            if (!e.className.includes("visible")) {e.className += " visible";}
+        } else {
+            if (e.className.includes("visible")) {e.className = e.className.replaceAll("visible","").trim();}
+            if (t.className.includes("invalid")) {t.className = t.className.replaceAll("invalid","").trim();}
+        }
+        r[n] = v;
+    }
+    return r;
+}
+
+function toSave() {
+    return {
+        data: toSaveFields(),
+        keys: toSaveKeys(),
+    };
+}
+
+function newRow() {
+    var td,i, tr = gm.newItem("tr","row-tr"),j,jm = base.fields.length,f,cn,def;
+    
+    for(j = 0;j < jm;j++) {
+        f = base.fields[j];
+        def = f.default_value;
+        cn = "field-" + f.name;
+        if (f.type === "boolean") {
+            td = gm.newItem("td",{
+                style: "text-align: center;"
+            },tr);
+        } else {
+            td = gm.newItem("td",{},tr);
+        }
+
+        if (f.values) {
+            i = gm.newItem("select",{className: cn,onchange: refresh},td);
+            f.values.forEach(function (val) {
+                if (typeof(val) == "object") {
+                    gm.newItem("option",{
+                        value: val.value,
+                        innerText: val.label
+                    },i);
+                } else {
+                    gm.newItem("option",{
+                        value: val,
+                        innerText: val
+                    },i);
+                }
+            });
+            if (def) {i.value = def;}
+        } else {
+            if (f.type === "number") {
+                if (!def) {def = "";}
+                i = gm.newItem("input",{
+                    type: "number",
+                    value: def,
+                    placeholder: f.placeholder,
+                    className: cn,
+                    onkeyup: refresh
+                },td);
+                if (f.required) {i.setAttribute("required","required");}
+            } else if (f.type === "boolean") {
+                i = gm.newItem("input",{
+                    type: "checkbox",
+                    className: cn,
+                    onclick: refresh,
+                    onchange: refresh
+                },td);
+                if (def === true) {i.checked = true;}
+            } else {
+                if (!def) {def = "";}
+                i = gm.newItem("input",{
+                    type: "text",
+                    value: def,
+                    placeholder: f.placeholder,
+                    className: cn,
+                    onkeyup: refresh
+                },td);
+                if (f.required) {i.setAttribute("required","required");}
+            }
+        }
+
+        gm.newItem("span",{
+            className: "error",
+            innerText: "This field is invalid"
+        },td);
+    }
+
+
+    (function () {
+        var bt;
+        var td = gm.newItem("td",{style: "width: 1px;"},tr);
+        bt = gm.newItem("button",{
+            className: "delete-button",
+            innerText: "Delete",
+            type: "button"
+        },td);
+        bt.addEventListener("click",function () {deleteRow(bt);});
+    })();
+    td = gm.newItem("td",{style: "width: 1px;"},tr);
+    (function () {
+        var bt;
+        bt = gm.newItem("button",{
+            className: "arrow-button",
+            innerHTML: "&uArr;",
+            type: "button"
+        },td);
+        bt.addEventListener("click",function () {swapUp(getRow(bt));;});
+    })();
+
+    (function () {
+        var bt;
+        bt = gm.newItem("button",{
+            className: "arrow-button",
+            innerHTML: "&dArr;",
+            type: "button"
+        },td);
+        bt.addEventListener("click",function () {swapDown(getRow(bt));;});
+    })();
+
+    return tr;
+}
+function getRow(child) {
+    var p = child;
+    while (!p.className.includes("row-tr") && p) {
+        p = p.parentElement;
+    }
+    return p;
+}
+function deleteRow(caller) {
+    var p = getRow(caller);
+    p.remove();
+    refresh();
+}
+
+function invalidFieldsCount() {
+    return document.querySelectorAll("#table .invalid-row").length;
+}
+
+function addRow() {
+    var t = document.getElementById("table");
+    var te = t.getElementsByTagName("tbody")[0].getElementsByClassName("row-tr")[0];
+    var r = newRow();
+    if (te) {
+        te.parentNode.insertBefore(r,te);
+    } else {
+        t.appendChild(r);
+    }
+    setTimeout(refresh,50);
+    return r;
+}
+
+function refresh() {
+    var b = document.getElementById("save_button");
+    var t = toSave(),iv = invalidFieldsCount();
+
+    if (iv === 0) {
+        if (b.className.includes("has-invalids")) {b.className = b.className.replaceAll("has-invalids","").trim();}
+        b.title = "";
+        b.innerText = "Save";
+    } else {
+        if (!b.className.includes("has-invalids")) {b.className += " has-invalids";}
+        b.title = "Save data: YOU will loose data";
+        b.innerText = "Save - Lossing " + String(iv) + " row" + ((iv > 1) ? "s" : "");
+    }
+
+    if (gm.deepEqual(firstSave,t)) {
+        b.disabled = true;
+    } else {
+        b.disabled = false;
+    }
+}
+
+function save(confirmed) {
+    var iv = invalidFieldsCount();
+    if (iv > 0 && !confirmed) {
+        if (confirm("Are you sure you want to save ?\nYou are lossing " + String(iv) + " row" + ((iv > 1) ? "s that are" : " that is") + " invalid") === true) {
+            return save(true);
+        } else {
+            return;
+        }
+    }
+    document.getElementById("save_button").disabled = true;
+    var t = toSave();
+    t = JSON.stringify(t);
+    gm.request("/",{
+        method: "POST",
+        body: t,
+        headers: {
+            "content-type": "application/json"
+        },
+        notifier: not
+    },function (r) {
+        if (r.status === 1) {
+            not.addMessage("Saved data with success",null,3000,true);
+            firstSave = toSave();
+        }
+        refresh();
+    });
+}
+
+function swapUp(elm) {
+    var previous = findPrevious(elm);
+    if (previous && previous.className != "no-swap") {
+        elm.parentNode.insertBefore(elm, previous);
+    }
+    refresh();
+}
+
+
+function findPrevious(elm) {
+   do {
+       elm = elm.previousSibling;
+   } while (elm && elm.nodeType != 1);
+   return elm;
+}
+
+function findNext(elm) {
+   do {
+       elm = elm.nextSibling;
+   } while (elm && elm.nodeType != 1);
+   return elm;
+}
+
+function swapDown(elem) {
+	var prev = findNext(elem);
+	if (prev && prev.className != "no-swap") {
+		swapUp(prev);
+	}
+    refresh();
+}
+
+gm.onload(onload);
---
+++ simple-db/index.js
@@ -0,0 +1,396 @@
+const Util = require("./../util.js");
+var replITDB = null;
+var baseUI = null;
+/**
+ * A simple list databse
+ */
+function treatDBField(f) {
+    if (!f) return null;
+    if (typeof(f) !== "object") {
+        f = {name: f};
+    }
+    f = Object.assign({
+        name: null,
+        type: "string",
+        primer: null,
+        values: null,
+        default_value: null,
+        required: false,
+        stretch: false,
+        placeholder: null
+    },f);
+    if (!f.name) return null;
+    if (typeof(f.stretch) === "number") f.stretch += "px;";
+    if (typeof(f.placeholder) !== "string") f.placeholder = Util.firstUpper(f.name) + ":";
+    if (!!f.primer && typeof(f.primer) !== "function") f.primer = null;
+    return f;
+}
+
+/**
+ * @typedef {Object} DBField
+ * @property {string} name
+ * @property {string} [ty="string"]
+ * @property {Function} [primer=null]
+ * @property {Array} [values=null] A list of proposed values
+ * @property {any} [default_value=""] The default_value for the field if empty
+ * @property {boolean} [required=false]
+ * @property {boolean|number} [stretch=false]
+ * @property {string} [placeholder=name]
+ */
+
+class SimpleDB {
+
+    /**
+     * @param {Array<DBField|string>} fields The DB fields
+     * @param {string} [replToken=null] Make it false to not use repl.it db
+     * @param {string} [dbEntry="simple_db"] The entry id of the data in the database
+     * @param {Array<DBField|string>} [keyNames="simple_db"] The keys names
+     */
+    constructor(fields,replToken,dbEntry = "simple_db",keyNames = []) {
+        if (typeof(dbEntry) !== "string") dbEntry = "simple_db";
+        this.fields = [];
+        this.keyNames = [];
+        if (!Array.isArray(keyNames)) keyNames = [];
+
+        for(let f of fields) {
+            let t = treatDBField(f);
+            if (t) this.fields.push(t);
+        }
+
+        for(let f of keyNames) {
+            let t = treatDBField(f);
+            if (t) this.keyNames.push(t);
+        }
+
+        
+
+        if (!this.fields.length) {
+            throw "Must provide fields for SimpleDB"
+        }
+        Object.defineProperty(this,"saveTimer",{writable: true,value: null});
+        Object.defineProperty(this,"last_save",{writable: true,value: null});
+        Object.defineProperty(this,"db",{writable: true,value: null});
+
+        this.primer = null;
+
+        let bd = "@replit/database";
+        if (replToken !== false) {
+            if (!replITDB) replITDB = require(bd);
+            this.db = new replITDB(replToken);
+        }
+        Object.defineProperty(this,"map_",{writable: true,value: null});
+
+        /**
+         * The field by which to map the data if asked
+         * @type {string}
+         */
+        this.mapBy = null;
+
+        /**
+         * The entry name in the database
+         * @type {string}
+         */
+        this.dbEntry = dbEntry;
+
+        /**
+         * The db keys, which are single values
+         * @type {Object<string,any>}
+         */
+        this.keys = {};
+
+        /**
+         * The db data
+         * @type {Array<Objct>}
+         */
+        this.data = [];
+
+    }
+
+    toSave() {
+        return {
+            data: this.data,
+            keys: this.keys
+        };
+    }
+
+    get map() {
+        if (this.map_ !== null) return this.map_;
+        return this.reMap();
+    }
+
+    reMap() {
+        let par = this;
+        if (!this.mapBy) return null;
+        this.map_ = this.data.reduce((map, obj,i) => {
+            map[obj[this.mapBy]] = obj;
+            return map;
+        }, {});
+        return this.map_;
+    }
+
+    start(clb) {
+        let par = this;
+        if (typeof(clb) !== "function") clb = function () {};
+        return this.fetchData().then(function () {
+            par.saveTimer = setInterval(function () {par.save();},2500);
+            par.map_ = null;
+            clb(par);
+        }).catch(console.error);
+    }
+
+    validateSingleValue(f,v) {
+        if (typeof(v) !== f.type) return false;
+        if (f.required) {
+            if (v === "" && f.type == "string") return false;
+        }
+        if (f.values) {
+            if (!f.values.includes(v)) return false;
+        }
+        return true;
+    }
+
+    validateValue(val) {
+        return this.fields.every(f => this.validateSingleValue(f,val[f.name]));
+    }
+    
+    validateData() {
+        let fn = this.validateValue.bind(this);
+        this.data.filter(fn);
+        this.map_ = null;
+    }
+
+    fetchData() {
+        return new Promise((resolve,reject) => {
+            this.db.get(this.dbEntry).then((r) => {
+                if (typeof(r) !== "object" || r === null) r = {data: [],keys: {}};
+                if (!Array.isArray(r.data)) r.data = [];
+                this.data = r.data;
+                if (typeof(r.keys) !== "object") r.keys = {};
+                this.keys = r.keys;
+
+                this.last_save = this.toSave();
+                this.refreshKeys();
+                this.map_ = null;
+                return resolve(this);
+            }).catch(reject);
+        });
+    }
+
+    refreshKeys() {
+        let ns = [];
+        for(let k of this.keyNames) {
+            let n = k.name;
+            if (n in this.keys) {
+                if (!this.validateSingleValue(k,this.keys[n])) this.keys[n] = k.default_value;
+            } else {
+                this.keys[n] = k.default_value;
+            }
+            ns.push(n);
+        }
+        for(let n in this.keys) {if (!ns.includes(n)) {delete this.keys[n];}}
+    }
+
+    get needsSave() {
+        return !Util.deepEqual(this.last_save,this.toSave());
+    }
+
+    toSaveIfNeed() {
+        let par = this,d = this.toSave();
+        if (Util.deepEqual(this.last_save,d) === true) return null;
+        return d;
+    }
+
+    save(force) {
+        let par = this,d = (force === true) ? this.toSave() : this.toSaveIfNeed();
+        if (!d) return;
+        return this.db.set(this.dbEntry,d).then(() => {par.map_ = null;par.last_save = d;});
+    }
+
+    UIHeaders() {
+        let html = ``;
+        for(let f of this.fields) {
+            if (f.stretch === false) {
+                html += `<th style="width: 1px;">`;
+            } else if (f.stretch !== true) {
+                html += `<th style="${f.stretch}">`;
+            } else {html += '<th>';}
+            html += `${Util.firstUpper(f.name)}${(f.required === true) ? '<span style="color: red;font-weight: bold;"> *</span>' : ""}${(this.mapBy === f.name) ? '<span style="color: red;font-weight: bold;"> (try unique values)</span>' : ""}</th>`;
+        }
+        return html + '<th style="width: 1px;"></th><th style="width: 1px;"></th>';
+    }
+
+    UIInput(f,value = "",prefix = "field") {
+        if (f.type == "number" && value === null) value = "";
+        let e = '<span class="error">This field is invalid</e>';
+        let re = (f.required) ? " required" : "",cl = `class="${prefix}-${f.name}" placeholder="${f.placeholder}"`;
+        if (f.values) {
+            let h = '';
+            for(let v of f.values) {
+                if (typeof(v) == "object") {
+                    h += `<option value="${v.value}">${v.label}</option>`;
+                } else {
+                    h += `<option value="${v}">${v}</option>`;
+                }
+            }
+            return `<select onchange="refresh();" ${cl} value=${value}>${h}</select>` + e;
+        }
+        if (f.type == "number") {
+            return `<input ${cl} type="number" onkeyup="refresh();" value="${value}"${re}>` + e;
+        } else if (f.type == "boolean") {
+            return `<input ${cl} onchange="refresh();" onclick="refresh();" type="checkbox"${value === true ? ' checked="checked"' : ""}${re}>` + e;
+        } else {
+            return `<input ${cl} type="text" onkeyup="refresh();" value="${value}"${re}>` + e;
+        }
+    }
+
+    UIItemRow(vals) {
+        let h = '<tr class="row-tr">';
+        for(let f of this.fields) {
+            if (f.type == "boolean") {
+                h += '<td style="text-align: center;">' + this.UIInput(f,vals[f.name]) + "</td>";
+            } else {
+                h += '<td>' + this.UIInput(f,vals[f.name]) + "</td>";
+            }
+        }
+        return h + `<td style="width: 1px;"><button class="delete-button" type="button" onclick="deleteRow(this);">Delete</button></td><td style="width: 1px;">
+                <button type="button" class="arrow-button" onclick="swapUp(getVideoRow(this));">&uArr;</button>
+                <button type="button" class="arrow-button" onclick="swapDown(getVideoRow(this));">&dArr;</button>
+            </td>` + "</tr>";
+    }
+
+    UIKeyRow(keyName,value = "") {
+        return `<tr class="key-row-tr"><td style="width: 1px"><p class="key-label">${Util.firstUpper(keyName.name)}${(keyName.required === true) ? '<span style="color: red;font-weight: bold;"> *</span>' : ""}</p></td><td>${this.UIInput(keyName,value,"key")}</td></tr>`;
+    }
+
+    UIItems() {
+        let h = '';
+        for(let v of this.data) {
+            h += this.UIItemRow(v);
+        }
+        return h;
+    }
+
+    UIKeys() {
+        this.refreshKeys();
+        let h = '';
+        for(let k of this.keyNames) {
+            h += this.UIKeyRow(k,this.keys[k.name]);
+        }
+        return h;
+    }
+
+    UI() {
+        return new Promise((resolve,reject) => {
+            if (!baseUI) {
+                Util.readFile(__dirname + "/index.html",true).then((d) => {
+                    baseUI = d;
+                    this.UI().then(resolve).catch(reject);
+                }).catch(reject);
+            } else {
+                let html = baseUI;
+                let headers = this.UIHeaders();
+                let base = {
+                    fields: this.fields,
+                    keyNames: this.keyNames
+                };
+
+                html = html.replace("{base}",JSON.stringify(base));
+                html = html.replace("{headers}",headers);
+                html = html.replace("{items}",this.UIItems());
+                html = html.replace("{keys}",this.UIKeys());
+                html = html.split("{title}").join((this.dbEntry === "simple_db") ? "" : " - " + this.dbEntry);
+
+                return resolve(html);
+            }
+        });
+    }
+
+    treatData(d) {
+        for(let f of this.fields) {
+            if (f.primer) d[f.name] = f.primer(d[f.name]);
+        }
+        if (this.primer) d = this.primer(d);
+        return d;
+    }
+
+    rePrimer() {
+        this.data = this.data.map(d => this.treatData(d));
+    }
+
+    setData(d) {
+        let par = this;
+        if (!Array.isArray(d)) return false;
+        d = d.filter(da => this.validateValue(da)).map(da => this.treatData(da));
+        this.data = d;
+        this.map_ = null;
+        return true;
+    }
+
+    setKeys(d) {
+        if (d === null || typeof(d) !== "object") return false;
+        this.keys = d;
+        this.refreshKeys();
+        return true;
+    }
+
+    processPost(req,res) {
+        let j = req.body;
+        if (!j) {
+            res.status(400);
+            return res.send("Missing request body");
+        }
+        if (typeof(j) !== "object") {
+            j = gm.JSONParse(j);
+            if (j === null) {
+                res.status(400);
+                return res.send("Invalid request body");
+            }
+        }
+        if (!j.keys || !j.data) {
+            res.status(400);
+            return res.send("Invalid request (missing params)");
+        }
+        if (!Array.isArray(j.data)) {
+            res.status(400);
+            return res.send("Invalid request body.data (must be array)");
+        }
+        if (typeof(j.keys) !== "object") {
+            res.status(400);
+            return res.send("Invalid request body.keys (must be object)");
+        }
+        this.setData(j.data);
+        this.setKeys(j.keys);
+        res.status(200);
+        return res.send("Saved data");
+    }
+
+    router(express) {
+        let router = express.Router();
+        let par = this;
+        router.get("/",function (req,res) {
+            par.UI().then((d) => {res.send(d);}).catch(function (e) {
+                console.error(e);
+                res.status(500);
+                return res.send(Util.errorPage(500,null,e));
+            });
+        });
+
+        router.post("/",function (req,res) {
+            return par.processPost(req,res);
+        });
+
+        router.get("/add.js",function (req,res) {
+            Util.readFile(__dirname + "/add.js").then((d) => {
+                res.send(d);
+            }).catch(function (e) {
+                res.status(500);
+                res.send(e);
+            });
+        });
+
+        return router;
+    }
+
+}
+
+module.exports = SimpleDB;
