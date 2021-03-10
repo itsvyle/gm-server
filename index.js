265fc320d59ff5a564a315c68289f0472a53e988
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-03-10 11:05:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
 	Collection: require("./collection.js"),
 	Util: require("./util.js"),
 	Sessions: require("./sessions.js"),
-	Flags: require("./flags.js"),
+	Flags: require("./flags.js")
 };
 
